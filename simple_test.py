e84898f8242046540704e2e8b91c56e853ad3f85
Daniel Dinjian <daniel.dinjian@tufts.edu>
2016-10-15 12:11:30 -0400

    Website displays meals recommended by server

--- simple_test.py
+++ simple_test.py
@@ -1,15 +1,41 @@
 f1=open('./FoodInfo.json', 'w+')
 f1.write('\
-  {"Meals":[\
-  {"BreakfastLowCal":[\
-    {"MainCourse": "Chicken Pattie",\
-     "SideDish": "Italian Salad",\
-     "Dessert": "Cake"\
-     }]\
+{"Breakfast":{\
+  "LowCal":{\
+    "MainCourse": ["Chicken Pattie", "325", "1", "5", "2"],\
+    "SideDish": ["Italian Salad", "254", "5", "2", "4"]},\
+  "MedCal":{\
+    "MainCourse": ["Chicken Nugget", "275", "4", "8", "19"],\
+    "SideDish": ["Unmedicated Pastrami", "86", "0", "31", "2"]},\
+  "HighCal":{\
+    "MainCourse": ["Big Mama", "666", "3", "2", "1"],\
+    "SideDish": ["More Pastrami", "86", "0", "31", "22"]}\
   },\
-  {"BreakfastMedCal":[\
-    {"MainCourse": "Fried Eggs",\
-     "SideDish": "Bacon and Potatoes",\
-     "Drink": "Lemon-infused Water"\
-    }]\
-  }]}')
+\
+"Lunch":{\
+  "LowCal":{\
+    "MainCourse": ["Chicken Pattie", "325", "1", "5", "2"],\
+    "SideDish": ["Italian Salad", "254", "5", "2", "4"]},\
+  "MedCal":{\
+    "MainCourse": ["Chicken Nugget", "275", "4", "8", "19"],\
+    "SideDish": ["Unmedicated Pastrami", "86", "0", "31", "2"]},\
+  "HighCal":{\
+    "MainCourse": ["Big Mama", "666", "3", "2", "1"],\
+    "SideDish": ["More Pastrami", "86", "0", "31", "22"]}\
+  },\
+\
+"Dinner":{\
+  "LowCal":{\
+    "MainCourse": ["Chicken Pattie", "325", "1", "5", "2"],\
+    "SideDish1": ["Italian Salad", "254", "5", "2", "4"],\
+    "SideDish2": ["Another Salad", "223", "5", "8", "1"]},\
+  "MedCal":{\
+    "MainCourse": ["Chicken Nugget", "275", "4", "8", "19"],\
+    "SideDish1": ["Unmedicated Pastrami", "86", "0", "31", "2"],\
+    "SideDish2": ["Another Salad", "223", "5", "8", "1"]},\
+  "HighCal":{\
+    "MainCourse": ["Big Mama", "666", "3", "2", "1"],\
+    "SideDish1": ["More Pastrami", "86", "0", "31", "22"],\
+    "SideDish2": ["Another Salad", "223", "5", "8", "1"]}\
+  }\
+ }')
