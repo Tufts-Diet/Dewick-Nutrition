c82f7193999801a7911551eef7dd8f6e46eb1487
NathanCAllen <NathanCAllen@users.noreply.github.com>
2016-10-15 02:48:07 -0400

    Update data_mine.py

--- data_mine.py
+++ data_mine.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib
 import re
-r = urllib.urlopen('http
+r = urllib.urlopen('http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts%20Dining&locationNum=11&locationName=Dewick-MacPhie%20Dining%20Center&naFlag=1').read()
+soup = BeautifulSoup(r)
+
+soup.find_all(onmouseover = "window.status = 'Click for Nutritive Analysis.'; return true;")
