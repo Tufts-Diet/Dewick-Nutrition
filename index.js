93662bf2a41cdbb7ef3a13b477cea0123e59ce6e
Daniel Dinjian <daniel.dinjian@tufts.edu>
2016-10-15 06:24:34 -0400

    Moved stylesheet into html, created Procfile for heroku, index.js serves webpage

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-//Lets require/import the HTTP module
-var http = require('http');
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
-app.use('/static', express.static(__dirname + '/public'));
+var path = require('path');
 
-//Lets define a port we want to listen to
-const PORT=8000; 
+const PORT=8000;
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.send(index.html);
-}
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
+app.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
