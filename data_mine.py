4c635b119848f4e1a411539e63a830e661976603
NathanCAllen <nathan@allenhub.com>
2016-10-15 03:03:29 -0400

    edit

--- data_mine.py
+++ data_mine.py
@@ -4,4 +4,8 @@ import re
 r = urllib.urlopen('http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts%20Dining&locationNum=11&locationName=Dewick-MacPhie%20Dining%20Center&naFlag=1').read()
 soup = BeautifulSoup(r)
 
-soup.find_all(onmouseover = "window.status = 'Click for Nutritive Analysis.'; return true;")
+result = soup.find_all(onmouseover = "window.status = 'Click for Nutritive Analysis.'; return true;")
+
+for meal in result:
+	print(meal.get('href'))
+
