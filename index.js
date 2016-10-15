5a818b9179015e14d85a722510d51ecb4ec84d40
Daniel Dinjian <daniel.dinjian@tufts.edu>
2016-10-15 07:06:59 -0400

    Set port in server

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ var app = express();
 var path = require('path');
 var PythonShell = require('python-shell');
 
+app.set('port', (process.env.PORT || 8000));
 
-const PORT=8000;
+var py_options = {
+scriptPath: '~/Desktop/Just\ for\ me/Dewick-Nutrition/'
+};
+
+PythonShell.run('data_mine.py', py_options, function (err, results) { 
+//your code (sets variables for files?)
+});
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
 });
 
 app.get('/BreakfastLowCal', function(req,res) {
@@ -54,4 +56,9 @@ app.get('/DinnerHighCal', function(req,res) {
 
 app.get('/Meals', function(req,res) {
 	
+});
+
+app.listen(app.get('port'), function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", app.get('port'));
 });
