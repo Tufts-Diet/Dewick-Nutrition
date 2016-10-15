f1ba1224647eb639a8adf3a842a10080961256d8
NathanCAllen <nathan@allenhub.com>
2016-10-15 03:04:45 -0400

    edit

--- data_mine.py
+++ data_mine.py
@@ -9,3 +9,5 @@ result = soup.find_all(onmouseover = "window.status = 'Click for Nutritive Analy
 for meal in result:
 	print(meal.get('href'))
 
+longmenu.asp?sName=Tufts+Dining&locationNum=11&locationName=Dewick%2DMacPhie+Dining+Center&naFlag=1&WeeksMenus=This+Week%27s+Menus&dtdate=10%2F15%2F2016&mealName=Dinner
+
