700b2bd8b32e29e48f73ec0a5e74b3268fac4b80
Daniel Dinjian <daniel.dinjian@tufts.edu>
2016-10-15 06:51:38 -0400

    Working server for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var PythonShell = require('python-shell');
+
 
 const PORT=8000;
 
@@ -12,4 +14,44 @@ app.get('/', function(req, res) {
 app.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+app.get('/BreakfastLowCal', function(req,res) {
+
+});
+
+app.get('/BreakfastMedCal', function(req,res) {
+
+});
+
+app.get('/BreakfastHighCal', function(req,res) {
+
+});
+
+app.get('/LunchLowCal', function(req,res) {
+	
+});
+
+app.get('/LunchMedCal', function(req,res) {
+	
+});
+
+app.get('/LunchHighCal', function(req,res) {
+	
+});
+
+app.get('/DinnerLowCal', function(req,res) {
+	
+});
+
+app.get('/DinnerMedCal', function(req,res) {
+	
+});
+
+app.get('/DinnerHighCal', function(req,res) {
+	
+});
+
+app.get('/Meals', function(req,res) {
+	
 });
